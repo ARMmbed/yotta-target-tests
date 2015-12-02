9794d5063f82f2a537f5c8773c8b38b93f80a4dd
Rob Moran <rob.moran@arm.com>
2015-12-02 17:17:17 +0000

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# yotta-target-tests
+## yotta Target build tests
+[![Circle CI](https://circleci.com/gh/ARMmbed/yotta-target-tests.svg?style=shield&circle-token=bd6f7481ee137c9cd26a8e38015db2df44573180)](https://circleci.com/gh/ARMmbed/yotta-target-tests)
+
 Tests to check each mbed-official gcc yotta target can build a project
+
+### Setup
+
+Use the [example circle yaml](example-circle.yml) to add yotta target builds to your project
+
+### What Happens with Circle CI
+
+* Installs the [yotta Docker image](https://hub.docker.com/r/mbed/yotta/) during build
+* npm installs the target test runner app during build
+* Uses runner app to run tests which lists targets from the yotta registry and builds each in turn, saving results to test_results.json
+* Offers test_results.json as an artifact
+
+### Development Prerequisites
+
+[Node.js](https://nodejs.org)
+
+[Docker](https://www.docker.com/)
---
+++ index.js
@@ -0,0 +1,152 @@
+// Imports
+var fs = require('fs');
+var path = require('path');
+var https = require('https');
+var spawn = require('child_process').spawn
+var readline = require('readline');
+
+// Variables
+var projectPath = path.resolve(process.cwd(), process.argv[2] || "./");
+var yottaCommand = "run --rm -e MBED_USER_ID=" + process.env.CIRCLE_USERNAME + " -v " + projectPath + ":/ytProject -w /ytProject --entrypoint yotta mbed/yotta";
+
+var queryHost = "registry.yottabuild.org";
+var queryPath = "keyword[]=mbed-official&query=gcc";
+var querySize = 30;
+
+var resultsFile = "./test_results.json";
+var resultLines = 10;
+var targets = [];
+
+function getTargets(completeFn, offset) {
+	offset = offset || 0;
+
+	var queryOptions = {
+		host: queryHost,
+		path: "/search?" + queryPath + "&limit=" + querySize + "&skip=" + offset
+	};
+
+	targetQuery(queryOptions, json => {
+		if (json.length === 0) completeFn();
+		else {
+			json = json.filter(module => {
+				return (module.type === "target");
+			});
+
+			json = json.map(module => {
+				return module.name;
+			});
+
+			targets = targets.concat(json);
+			getTargets(completeFn, offset += querySize);
+		}
+	});
+}
+
+function targetQuery(options, completeFn) {
+	https.request(options, res => {
+		var retval = "";
+
+		res.on('data', chunk => {
+			retval += chunk;
+		});
+
+		res.on('end', () => {
+			var json = JSON.parse(retval);
+			completeFn(json);
+		});
+	}).end();
+}
+
+function buildTarget(target) {
+	return new Promise(resolve => {
+		console.log("processing: " + target);
+		var deps = null;
+
+		function complete(passed, deps, debug) {
+			var result = {
+				target: target,
+				passed: passed
+			};
+
+			if (deps) result.deps = deps;
+			if (debug) result.debug = debug;
+			updateResults(result);
+
+			console.log("target " + target + " " + (passed ? "passed" : "failed"));
+			resolve();
+		}
+
+		yottaExec("target " + target)
+		.then(() => {
+			return yottaExec("list --json");
+		})
+		.then(data => {
+			deps = JSON.parse(data);
+			return yottaExec("clean");
+		})
+		.then(() => {
+			return yottaExec("build");
+		})
+		.then(() => {
+			complete(true, deps);
+		})
+		.catch(lines => {
+			complete(false, deps, lines.join("\n"));
+		});
+	});
+}
+
+function yottaExec(command) {
+	return new Promise((resolve, reject) => {
+		var args = yottaCommand.split(" ");
+		args = args.concat(command.split(" "));
+
+		var yt = spawn("docker", args);
+		var lines = [];
+
+		yt.stdout.on('data', data => {
+			console.log(data.toString());
+		});
+
+		yt.on("close", code => {
+			if (code === 0) {
+				resolve(lines);
+			} else {
+				reject(lines);
+			}
+		});
+
+		readline.createInterface({
+			input: yt.stdout
+		}).on("line", line => {
+			lines.push(line);
+			while (lines.length > resultLines) lines.shift();
+		});
+	});
+}
+
+function updateResults(result) {
+	var results = [];
+
+	if (fs.existsSync(resultsFile)) {
+		results = JSON.parse(fs.readFileSync(resultsFile));
+	}
+
+	results.push(result);
+	fs.writeFileSync(resultsFile, JSON.stringify(results));
+}
+
+getTargets(() => {
+	console.log("found " + targets.length + " targets matching '" + queryPath + "'");
+
+	if (fs.existsSync(resultsFile)) fs.unlinkSync(resultsFile);
+
+	targets.reduce((sequence, target) => {
+		return sequence.then(() => {
+			return buildTarget(target);
+		});
+	}, Promise.resolve())
+	.then(() => {
+		process.exit();
+	});
+});
